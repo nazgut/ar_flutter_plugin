a5f7a6144bdddc08c9131df19183400f86199ceb
Aleksander Majda <nazgut@gmail.com>
2023-06-28 23:20:59 +0200

    debug anchor

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -850,6 +850,7 @@ internal class AndroidARView(
             anchorNode.setParent(arSceneView.scene)
             true
         } catch (e: Exception) {
+            println("Error Anchor: $e")
             false
         }
     }
