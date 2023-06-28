5caf64358e6d559e9340d513fd514e78f6616000
Aleksander Majda <nazgut@gmail.com>
2023-06-28 22:49:14 +0200

    debug anchor

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -222,6 +222,7 @@ internal class AndroidARView(
                     when (call.method) {
                         "addAnchor" -> {
                             val anchorType: Int? = call.argument<Int>("type")
+                            println("Anchor Type: $anchorType")
                             if (anchorType != null){
                                 when(anchorType) {
                                     0 -> { // Plane Anchor
