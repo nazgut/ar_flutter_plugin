894867dd16d57c099527ba9b1aaa6e5a7d897a0d
Aleksander Majda <nazgut@gmail.com>
2023-06-29 10:47:02 +0200

    debug anchor

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -843,7 +843,9 @@ internal class AndroidARView(
     private fun addPlaneAnchor(transform: ArrayList<Double>, name: String): Boolean {
         return try {
             val position = floatArrayOf(deserializeMatrix4(transform).second.x, deserializeMatrix4(transform).second.y, deserializeMatrix4(transform).second.z)
+            println("%%%%%%%%%position: $position")
             val rotation = floatArrayOf(deserializeMatrix4(transform).third.x, deserializeMatrix4(transform).third.y, deserializeMatrix4(transform).third.z, deserializeMatrix4(transform).third.w)
+            println("%%%%%%%%%rotation: $rotation")
             val anchor: Anchor = arSceneView.session!!.createAnchor(Pose(position, rotation))
             println("%%%%%%%%%Anchor: $anchor")
             val anchorNode = AnchorNode(anchor)
